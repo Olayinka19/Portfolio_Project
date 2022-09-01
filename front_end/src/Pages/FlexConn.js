1b689434e1d7eb0404e109c39c650c63e9522f37
fakanbiomowunmi@gmail.com <fakanbiomowunmi@gmail.com>
2022-09-01 00:15:17 -0400

    Almost

--- front_end/src/Pages/FlexConn.js
+++ front_end/src/Pages/FlexConn.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import FlexConnect from '../Components/FlexConnect'
+import FlexConnect from '../Components/FlexConnect';
+import Spinner from 'react-bootstrap/Spinner';
 
 function FlexConn() {
   return (
     <div>
       <FlexConnect />
+      <Spinner animation="border" role="status" className='wallett'>
+        <span className='visually-hidden'>Loading...</span>
+      </Spinner>
+
+      <Spinner animation="border" role="status" className='wallett1'>
+        <span className='visually-hidden'>Loading...</span>
+      </Spinner>
+      <Spinner animation="grow"></Spinner>
     </div>
   )
 }
