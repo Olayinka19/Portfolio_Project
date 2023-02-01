cbfce0eac16db71bad616f1949ded9a55e709534
fakanbiomowunmi@gmail.com <fakanbiomowunmi@gmail.com>
2023-01-31 18:43:28 -0600

    Portfolio-project

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,19 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Edit from "./Pages/Edit";
 import FourOFour from "./Pages/FourOFour";
 import Home from "./Pages/Home";
-import Index from "./Pages/IndexPage";
-import New from "./Pages/New";
-import Show from "./Pages/Show";
 import NavBar from "./Components/NavBar";
 import Footer from "./Components/Footer";
 // import Wallet from "./Pages/AboutPage";
 import FlexConn from "./Pages/FlexConn";
 import AboutPage from "./Pages/AboutPage";
 import Profile from "./Components/Profile";
-import portfolioDetails from "./Components/PortfolioDetails";
 import Portfolios from "./Components/Portfolios";
-import Porfolio from "./Components/Portfolio";
 import Signin from "./Pages/Signin";
 import {AuthContextProvider} from './Context/AuthContext';
 import './App.css';
@@ -29,8 +23,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/Portfolios" element={<Portfolios />} />
         <Route path="/PortfoliosDetails" element={<PortfolioDetails />} />
-        <Route path="/products/:id/edit" element={<Edit />} />
-        <Route path="/products/new" element={<New />} />
         <Route path="/FlexConnect" element={<FlexConn />} />
         <Route path="/About" element={<AboutPage />} />
         <Route path="/Profile" element={<Profile />} />
