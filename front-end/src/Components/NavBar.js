455e694e6b92e0173fe5fec4cc87c292fe9faa33
fakanbiomowunmi@gmail.com <fakanbiomowunmi@gmail.com>
2023-03-01 20:27:17 -0600

    Fixed my display pictures and Logo

--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -71,15 +71,15 @@ export default function NavBar() {
               </Offcanvas.Header>
               <Offcanvas.Body>
                 <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="/">Home</Nav.Link>
+                  <Link to="/">Home</Link>
                   {/* <Link to="/Developer">Developer</Link> */}
-                  {/* <Nav.Link href="/Explore">Explore</Nav.Link> */}
+                  {/* <Link to="/Explore">Explore</Link> */}
                   {/* <link href="/Developer">Developer</link> */}
-                  <Nav.Link href="/developer">Developer</Nav.Link>
-                  <Nav.Link href="/men">Men</Nav.Link>
-                  <Nav.Link href="/about">About</Nav.Link>
-                  <Nav.Link href="/women">Women</Nav.Link>
-                  <Nav.Link href="/electronics">Electronics</Nav.Link>
+                  <Link to="/Developer">Developer</Link>
+                  <Link to="/Men">Men</Link>
+                  <Link to="/About">About</Link>
+                  <Link to="/Women">Women</Link>
+                  <Link to="/Electronics">Electronics</Link>
 
                   {user && user.displayName ? (
         <Button onClick={handleSignOut} variant="primary">LogOut</Button>
--- front-end/src/Pages/Account.js
+++ front-end/src/Pages/Account.js
@@ -4,19 +4,31 @@ import { Col , Button} from 'react-bootstrap/';
 import { Link } from 'react-router-dom';
 // import { UserAuth } from '../context/AuthContext';
 import { UserAuth } from '../Context/AuthContext';
+import Card from 'react-bootstrap/Card';
 
 export default function Account({ handleTextChange, handleNumberChange }) {
     const {user } = UserAuth();
     return (
         <div className='p-2'>
            
-           <div>
+           {/* <div>
         <p>Welcome, {user?.displayName}</p>
         <h3>{user?.email}</h3>
         <div>
           <img src={user?.photoURL} />
-        </div>
-      </div>
+        </div> */}
+      {/* </div> */}
+      <Card>
+      <Card.Header as="h5">Account Information</Card.Header>
+      <Card.Body>
+        <Card.Title>Welcome, {user?.displayName}</Card.Title>
+        <Card.Text>
+        {user?.email}.
+        </Card.Text>
+        <Card.Text><img width="90px" height="80px" border-radius="22px" src={user?.photoURL} /></Card.Text>
+        {/* <Button variant="primary"><GoogleButton onClick={handleGoogleSignIn} /></Button> */}
+      </Card.Body>
+    </Card>
         </div>
 
     )
