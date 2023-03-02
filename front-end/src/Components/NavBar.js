b1c2fcd8f792857f11baecbfc6cccab8016b7543
fakanbiomowunmi@gmail.com <fakanbiomowunmi@gmail.com>
2023-03-01 18:11:53 -0600

    Navbar

--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -8,6 +8,12 @@ import { NavDropdown } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import { UserAuth } from "../Context/AuthContext";
+// import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import Ahead from './Images/Ahead.png'
+
+
+
 export default function NavBar() {
 
 
@@ -22,117 +28,91 @@ export default function NavBar() {
   return (
     // <header>
 
-    <Navbar
-      collapseOnSelect
-      expand="lg"
-      // bg="dark"
-      // variant="dark"
-      className="Nav"
-    >
-    
-      <Container>
-        <Navbar.Brand>
-          <Link to="/"><Button variant="contained" color="success" to="/">
-  Home
-</Button></Link>
-          {/* <Link to="/" className="Link">
-            <img
-              src="https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F312b1033-01d7-4b1c-a075-64ca0c79b05c%2FOlayinka_Fakanbi.jpg?table=block&id=a5b8ac40-ed54-464d-9702-7dd5c24a0e22&spaceId=e2735e9b-8fe9-4c16-bdc1-be142cd1014e&width=2000&userId=1db7092b-3ec4-4da8-985b-7b836b0a69ad&cache=v2"
-              alt="logo"
-              className="img-logo"
-            />
-            
-
-          </Link> */}
-          
-        </Navbar.Brand>
-        <br></br>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-        
-            <Nav.Link href="/About">Blog</Nav.Link>
-            <Nav.Link href="/Explore">Explore</Nav.Link>
-            <Nav.Link href="/Men">Men</Nav.Link>
-            <Nav.Link href="/Women">Women</Nav.Link>
-            <Nav.Link href="/Electronics">Electronics</Nav.Link>
-            <Nav.Link href="/porfolios">Porfolio</Nav.Link>
-            <Nav.Link href="/Developer">Developer</Nav.Link>
-        
-            <Nav.Link
-              href="/PortfolioDetails"
-              
+    <>
+            {[false].map((expand) => (
+        <Navbar key={expand} bg="light" expand={expand} className="mb-3">
+          <Container fluid>
+            <Navbar.Brand href="/"><img width="120px" height="50px" src={Ahead} alt="React Logo" /></Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="side" className="offCanavNav"
             >
-              PortfolioDetails
-            </Nav.Link>
-            <form>
-      
-    </form>
-            
-    
-              
-    
-               
-<img src="https://cdn-icons-png.flaticon.com/512/1144/1144760.png" width="30px" height="30px" />
-            <NavDropdown title="Account" id="basic-nav-dropdown">
-            {user && user.displayName ? ( 
-                  
-                  <span className="displayname">   
-                  
-                  <img src={user.photoURL} alt="pfp" className="img-logo"/> 
-                  <p className="displayN">Hi,{user.displayName}</p>
-                  </span>
-      ) : null}  
-              <NavDropdown.Item>
-                <Link to="/FlexConnect" className="Link">
-                 <img alt="pfp" width="45px" height="45px" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Qd_PyuUdp9Se1AGdWWhXVwHaHa%26pid%3DApi&f=1&ipt=33b7eb916f31f77bd6dd76a51cb4d109caca8056b91d1f5c3d5b88bb42b30c0f&ipo=images"></img>
-                </Link>
-              </NavDropdown.Item>
-              <NavDropdown.Item>
-                <Link to="/Profile" className="Link">
-                  Profile
-                </Link>
-              </NavDropdown.Item>
-              <NavDropdown.Item>
-                <Link to="/Account" className="Link">
-                  Account
-                </Link>
-              </NavDropdown.Item>
-              <NavDropdown.Item>
-                <Link to="/DocsNeeded" className="Link">
-                  Documents
-                </Link>
-              </NavDropdown.Item>
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                {user && user.displayName ? (
+                      <div>
+                        <img
+                          style={{
+                            marginRight: "20px",
+                            borderRadius: "25px",
+                            width: "45px",
+                            height: "45px",
+                          }}
+                          alt="user-img"
+                          src={user && user.photoURL}
+                        />
 
-              <NavDropdown.Item>
-               
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-           
-          <Nav.Link eventKey={2}>
-          
-            <Form className="d-flex">
-              <Form.Control
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">SEARCH</Button>
-              <NavDropdown.Item>
-              {user && user.displayName ? (
+                        <p>Hi, {user.displayName}</p>
+                      </div>
+                    ) : (
+                      <Link to="/user/1/explore">
+                        <img
+                          src="https://cdn-icons-png.flaticon.com/512/5267/5267467.png"
+                          width="55px"
+                          height="55px"
+                        ></img>{" "}
+                      </Link>
+                    )}
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link href="/">Home</Nav.Link>
+                  <Nav.Link href="/Explore">Explore</Nav.Link>
+                  <Nav.Link href="/Developer">Developer</Nav.Link>
+                  <Nav.Link href="/Men">Men</Nav.Link>
+                  <Nav.Link href="/About">About</Nav.Link>
+                  <Nav.Link href="/Women">Women</Nav.Link>
+                  <Nav.Link href="/Electronics">Electronics</Nav.Link>
+
+                  {user && user.displayName ? (
         <Button onClick={handleSignOut}>LogOut</Button>
       ) : (
         <Link to='/signin'><Button variant="warning">Log In</Button></Link>
       )}
-              </NavDropdown.Item>
-    
-            </Form>
-          </Nav.Link>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    // </header>
+                  <NavDropdown
+                    title="Dropdown"
+                    id={`offcanvasNavbarDropdown-expand-${expand}`}
+                  >
+                    <NavDropdown.Item href="/Account">Account</NavDropdown.Item>
+                    <NavDropdown.Item href="/Profile">
+                      Profile
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action5">
+                      Something else here
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+                <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button variant="outline-success">Search</Button>
+                </Form>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+
+    </>
+
+
   );
 }
--- front-end/src/Pages/Account.js
+++ front-end/src/Pages/Account.js
@@ -1,143 +1,22 @@
 import React from 'react';
 import { Form } from 'react-bootstrap/';
 import { Col , Button} from 'react-bootstrap/';
+import { Link } from 'react-router-dom';
+// import { UserAuth } from '../context/AuthContext';
+import { UserAuth } from '../Context/AuthContext';
 
 export default function Account({ handleTextChange, handleNumberChange }) {
+    const {user } = UserAuth();
     return (
         <div className='p-2'>
-            <div className="container">
-                <div className="border border-dark">
-                    <h4 className="text-center m-3">Personal Information</h4>
-                    {/* Name */}
-                    <Form.Group className="m-3 userFullName" >
-                        <Form.Label>Full Name</Form.Label>
-                        <Form.Control
-                            id="name"
-                            placeholder="Enter Your Full Name"
-                            onChange={handleTextChange}
-                        // required
-                        />
-                    </Form.Group>
-
-                    {/* Age */}
-                    <Form.Group className="m-3 userAge">
-                        <Form.Label>Age</Form.Label>
-                        <Form.Control
-                            id="age"
-                            type="number"
-                            placeholder="Enter age"
-                            onChange={handleNumberChange}
-                        // required
-                        />
-                    </Form.Group>
-
-                    {/* Sex */}
-                    <div className="m-3 sex-div-outter" onChange={handleTextChange}>
-                        <Form.Label>Sex</Form.Label>
-                        <div className="p-3 sex-div-inner border">
-                            <Form.Check
-                                label='Male'
-                                name='sex'
-                                type="radio"
-                                id="sex"
-                                value="male"
-                            />
-                            <Form.Check
-                                label='Female'
-                                name='sex'
-                                type="radio"
-                                id="sex"
-                                value="female"
-                            />
-                            <Form.Check
-                                label='Prefer Not to Say'
-                                name='sex'
-                                type="radio"
-                                id="sex"
-                                value="null"
-                            />
-                        </div>
-                    </div>
-
-                    {/* Income */}
-                    {/* <Form.Group className="m-3 userIncome">
-                        <Form.Label>Annual Income</Form.Label>
-                        <Form.Control
-                            id="income"
-                            type="number"
-                            placeholder="Enter Your Net Income"
-                            onChange={handleNumberChange}
-                        // required
-                        />
-                    </Form.Group> */}
-
-                    <Form.Group as={Col} controlId="state">
-                                <Form.Label>State</Form.Label>
-                                <Form.Control
-                                    as="select"
-                                    name="state"
-                                    // onChange={handleAddressTextChange}
-                                // required
-                                >
-                                    <option value="">Select State</option>
-                                    <option value="AL">Alabama</option>
-                                    <option value="AK">Alaska</option>
-                                    <option value="AZ">Arizona</option>
-                                    <option value="AR">Arkansas</option>
-                                    <option value="CA">California</option>
-                                    <option value="CO">Colorado</option>
-                                    <option value="CT">Connecticut</option>
-                                    <option value="DE">Delaware</option>
-                                    <option value="DC">District Of Columbia</option>
-                                    <option value="FL">Florida</option>
-                                    <option value="GA">Georgia</option>
-                                    <option value="HI">Hawaii</option>
-                                    <option value="ID">Idaho</option>
-                                    <option value="IL">Illinois</option>
-                                    <option value="IN">Indiana</option>
-                                    <option value="IA">Iowa</option>
-                                    <option value="KS">Kansas</option>
-                                    <option value="KY">Kentucky</option>
-                                    <option value="LA">Louisiana</option>
-                                    <option value="ME">Maine</option>
-                                    <option value="MD">Maryland</option>
-                                    <option value="MA">Massachusetts</option>
-                                    <option value="MI">Michigan</option>
-                                    <option value="MN">Minnesota</option>
-                                    <option value="MS">Mississippi</option>
-                                    <option value="MO">Missouri</option>
-                                    <option value="MT">Montana</option>
-                                    <option value="NE">Nebraska</option>
-                                    <option value="NV">Nevada</option>
-                                    <option value="NH">New Hampshire</option>
-                                    <option value="NJ">New Jersey</option>
-                                    <option value="NM">New Mexico</option>
-                                    <option value="NY">New York</option>
-                                    <option value="NC">North Carolina</option>
-                                    <option value="ND">North Dakota</option>
-                                    <option value="OH">Ohio</option>
-                                    <option value="OK">Oklahoma</option>
-                                    <option value="OR">Oregon</option>
-                                    <option value="PA">Pennsylvania</option>
-                                    <option value="RI">Rhode Island</option>
-                                    <option value="SC">South Carolina</option>
-                                    <option value="SD">South Dakota</option>
-                                    <option value="TN">Tennessee</option>
-                                    <option value="TX">Texas</option>
-                                    <option value="UT">Utah</option>
-                                    <option value="VT">Vermont</option>
-                                    <option value="VA">Virginia</option>
-                                    <option value="WA">Washington</option>
-                                    <option value="WV">West Virginia</option>
-                                    <option value="WI">Wisconsin</option>
-                                    <option value="WY">Wyoming</option>
-                                </Form.Control>
-                            </Form.Group>
-                            <br></br>
-                            {/* <Button type="submit" class="btn btn-" to="/">Submit</Button> */}
-                            <a href="/"><Button>Home</Button></a>
-                </div>
-            </div>
+           
+           <div>
+        <p>Welcome, {user?.displayName}</p>
+        <h3>{user?.email}</h3>
+        <div>
+          <img src={user?.photoURL} />
+        </div>
+      </div>
         </div>
 
     )
