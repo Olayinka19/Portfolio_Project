6a24ac240af1c9fd0fcc930b8a00a6372cfde53c
fakanbiomowunmi@gmail.com <fakanbiomowunmi@gmail.com>
2023-03-04 02:58:47 -0600

    Aheadshopping

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -12,7 +12,8 @@ import Animation from "../Components/Animation";
 // import PortfolioDetails from "../Components/PortfolioDetails";
 // import Portfolios from "../Components/Portfolios";
 // import { useState, useEffect } from "react";
-
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
 
 
 const API = process.env.REACT_APP_API_URL;
@@ -96,7 +97,20 @@ function Home() {
       
       <br/>
 
+      <ImageList sx={{ width: 390, height: 450 }} cols={3} variant="woven" rowHeight={164}>
+      {itemData2.map((item) => (
+        <ImageListItem key={item.img}>
+          <img
+            src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+            srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+            alt={item.title}
+            loading="lazy"
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
 
+    
   
       <hr/>
 
@@ -121,8 +135,21 @@ Merrycolor Boho Lumbar Throw Pillow Cover 12x20
       </Card.Body>
     </Card>
     </div>
+    <ImageList sx={{ width: 390, height: 450 }}  cols={3} gap={8}>
+      {itemData.map((item) => (
+        <ImageListItem key={item.img}>
+          <img
+            src={`${item.img}?w=161&fit=crop&auto=format`}
+            srcSet={`${item.img}?w=161&fit=crop&auto=format&dpr=2 2x`}
+            alt={item.title}
+            loading="lazy"
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+ 
       <br/>
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <div style={{ justifyContent: 'center' }}>
     <Card className="text-center">
       <Card.Header>Featured</Card.Header>
       <Card.Img variant="top" src="https://m.media-amazon.com/images/I/8101kmkFW0L._AC_SL1500_.jpg" />
@@ -131,7 +158,7 @@ Merrycolor Boho Lumbar Throw Pillow Cover 12x20
         {/* <Card.Text>
           With supporting text below as a natural lead-in to additional content.
         </Card.Text> */}
-        <Button variant="warning">Sales</Button>
+        <Button variant="warning"><b>$350 </b><span font-size="5px"><del>200</del></span></Button>
       </Card.Body>
       <Card.Footer className="text-muted">2 days ago</Card.Footer>
     </Card>
@@ -139,13 +166,13 @@ Merrycolor Boho Lumbar Throw Pillow Cover 12x20
 
     <Card className="text-center">
       <Card.Header>Featured</Card.Header>
-      <Card.Img variant="top" src="https://m.media-amazon.com/images/I/61s7RGRwSoL._AC_SL1500_.jpg" />
+      <Card.Img variant="top" src="https://m.media-amazon.com/images/I/71rtUgIGEhL._AC_SX679_.jpg" />
       <Card.Body>
         <Card.Title>BOHON LED Desk Lamp</Card.Title>
         {/* <Card.Text>
           With supporting text below as a natural lead-in to additional content.
         </Card.Text> */}
-        <Button variant="warning">Sales</Button>
+        <Button variant="warning"><b>$46.67 </b><span fontSize="10px"><del>$49.99</del></span></Button>
       </Card.Body>
       <Card.Footer className="text-muted">2 days ago</Card.Footer>
     </Card>
@@ -158,7 +185,7 @@ Merrycolor Boho Lumbar Throw Pillow Cover 12x20
         {/* <Card.Text>
           With supporting text below as a natural lead-in to additional content.
         </Card.Text> */}
-        <Button variant="warning">Sales</Button>
+        <Button variant="warning"><b>$350 </b><span fontSize="10px"><del>200</del></span></Button>
       </Card.Body>
       <Card.Footer className="text-muted">2 days ago</Card.Footer>
     </Card>
@@ -188,32 +215,30 @@ Merrycolor Boho Lumbar Throw Pillow Cover 12x20
             20% off Gaming Laptop on Sale!!!
           </Card.Text>
         </Card.Body>
+        <Card.Img variant="bottom" src="https://m.media-amazon.com/images/I/71m-r6CHobL._AC_UL1500_.jpg" />
       </Card>
       <br />
       <Card>
         <Card.Body>
           <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            Adidas
           </Card.Text>
         </Card.Body>
-        <Card.Img variant="bottom" src="https://img.freepik.com/free-psd/food-menu-restaurant-social-media-cover-template_120329-1339.jpg?t=st=1675662336~exp=1675662936~hmac=ce3c7216c0b3db561d25df9a17643642b95c5ab592126d85f9f36d46fdec291d" />
+        <Card.Img variant="bottom" src="https://m.media-amazon.com/images/S/mms-media-storage-prod/final/BrandPosts/brandPosts/b13c180d-c494-4596-b5c6-9ddd1f83ccf7/bc145784-3faf-4242-8788-4c25e2c3937f/media._SL850_FMjpg_.jpeg" />
       </Card>
    </div>
    <Card className="text-center">
-      <Card.Header>Top Deal</Card.Header>
+      <Card.Header>DREAM PAIRS</Card.Header>
       <Card.Body>
-        <Card.Title>50% off Special Offer for Just You!!</Card.Title>
+        <Card.Title>Soccer Shoes</Card.Title>
         <Card.Text>
-          With supporting text below as a natural lead-in to additional content.
+        Men's Firm Ground Soccer Cleats
         </Card.Text>
-        <Card.Img variant="bottom" src="https://img.freepik.com/premium-psd/food-menu-restaurant-social-media-post-banner-template-design_502432-42.jpg?w=1060" />
-  
-        <Button variant="warning">Sales</Button>
-        
+        <Card.Img variant="bottom" src="https://m.media-amazon.com/images/I/81-QcRvPZlL._AC_UL1500_.jpg" />
+        <Button variant="warning"><b>$37.99 </b><span fontSize="10px"><del>$42.99</del></span></Button>
       </Card.Body>
-      {/* <Card.Footer className="text-muted">2 days ago</Card.Footer> */}
-      
+      <Card.Img variant="bottom" src="https://m.media-amazon.com/images/I/61BeIFQYb+L._AC_SX679_.jpg" />
+      {/* <Card.Footer className="text-muted">2 days ago</Card.Footer> */} 
     </Card>
 
 
@@ -222,7 +247,7 @@ Merrycolor Boho Lumbar Throw Pillow Cover 12x20
    </div>
    <hr></hr>
 <div style ={{display: 'flex', justifyContent: 'center' }}>
-   <Card style={{ width: '48rem' }}>
+   <Card style={{ width: '28rem' }}>
       <Card.Img variant="top" src="https://m.media-amazon.com/images/I/71+Uwfg5yjL._AC_SL1500_.jpg" />
       <Card.Body>
         <Card.Title>Card Title</Card.Title>
@@ -340,13 +365,113 @@ Merrycolor Boho Lumbar Throw Pillow Cover 12x20
       </Card>
     </div>
     <hr></hr>
+    
     {/* <p>For general questions, contact me at <a href="mailto:olayinkafakanbi@pursuit.org" target="_blank" rel="noreferrer"><b>Olayinkafakanbi@pursuit.org</b></a> I'll get back to you within 1 business day. For business refereral and ther professional purposes.</p> */}
     </div>
     </div>
     </>
   );
 }
-
+const itemData = [
+  {
+    img: 'https://m.media-amazon.com/images/I/81bZ-ZaQb9L._AC_SX679_.jpg',
+    title: 'Tide Pods',
+  },
+  {
+    img: 'https://m.media-amazon.com/images/I/61gaNdRj0OL._SL1500_.jpg',
+    title: 'BWater Wipes',
+  },
+  {
+    img: 'https://m.media-amazon.com/images/I/71c3wsV98rL.__AC_SX300_SY300_QL70_FMwebp_.jpg',
+    title: 'Baby Wipes',
+  },
+  {
+    img: 'https://m.media-amazon.com/images/I/91edFRbvyIL._AC_UY436_FMwebp_QL65_.jpg',
+    title: 'Coffee',
+  },
+  {
+    img: 'https://m.media-amazon.com/images/I/41q91ld1d6L._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
+    title: 'Hats',
+  },
+  {
+    img: 'https://m.media-amazon.com/images/I/51lGq+i-BKL._SY344_BO1,204,203,200_.jpg',
+    title: 'Honey',
+  },
+  {
+    img: 'https://m.media-amazon.com/images/I/61aQPrW-+rL._AC_UL640_QL65_.jpg',
+    title: 'Basketball',
+  },
+  {
+    img: 'https://m.media-amazon.com/images/I/61S+-V9lowL._AC_UY436_QL65_.jpg',
+    title: 'Fern',
+  },
+  {
+    img: 'https://m.media-amazon.com/images/I/71h37mxNQmL._AC_UY436_FMwebp_QL65_.jpg',
+    title: 'Mushrooms',
+  },
+  {
+    img: 'https://m.media-amazon.com/images/I/41ZjUOH6nRL._AC_UL640_FMwebp_QL65_.jpg',
+    title: 'Tomato basil',
+  },
+  {
+    img: 'https://m.media-amazon.com/images/I/71S1Cyt9QhL._AC_UL640_FMwebp_QL65_.jpg',
+    title: 'Sea star',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+    title: 'Bike',
+  },
+];
+const itemData2 = [
+  {
+    img: 'https://images.unsplash.com/photo-1549388604-817d15aa0110',
+    title: 'Bed',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1563298723-dcfebaa392e3',
+    title: 'Kitchen',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1523413651479-597eb2da0ad6',
+    title: 'Sink',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1525097487452-6278ff080c31',
+    title: 'Books',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1574180045827-681f8a1a9622',
+    title: 'Chairs',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62',
+    title: 'Candle',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1530731141654-5993c3016c77',
+    title: 'Laptop',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1481277542470-605612bd2d61',
+    title: 'Doors',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1517487881594-2787fef5ebf7',
+    title: 'Coffee',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee',
+    title: 'Storage',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1519710164239-da123dc03ef4',
+    title: 'Coffee table',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1588436706487-9d55d73a39e3',
+    title: 'Blinds',
+  },
+];
 
 
 export default Home;
